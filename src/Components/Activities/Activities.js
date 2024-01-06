bdb6910835899e700c2b74f416c3e31aabb30798
unknown <upendrapanchali83@gmail.com>
2024-01-06 12:45:02 +0530

    started the vis

---
+++ src/Components/Activities/Activities.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Activities.css';
+
+function Activities(){
+    return(
+        <>
+        <h1>Activities</h1>
+        </>
+    );
+}
+
+export default Activities;
