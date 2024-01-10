a29fa9f17cf4e5afde7b324e30d651a70dbc2a46
unknown <upendrapanchali83@gmail.com>
2024-01-10 11:58:00 +0530

    admission form completed

--- src/Components/Admissions/Admissions.js
+++ src/Components/Admissions/Admissions.js
@@ -26,36 +26,130 @@ function Admissions(){
                 </div>
             </div>
             <div className='admissions-form'>
-                <div>
-                    <h1>Admission Enquiry</h1>
+                <div className='admission-form-con'>
+                    <h1 className='admission-form-con-header'>Admission Enquiry</h1>
+                    {/* admission enquiry first part */}
+                    <div className='adm-enq-fst'>
+                        <div className='adm-enq-fst-div1'>
+                            <label>Academic Year <span>*</span></label>
+                            <select>
+                                <option>2024-2023</option>
+                            </select>
+                        </div>
+                        <div className='adm-enq-fst-div1'>
+                            <label>Class/Standard <span>*</span></label>
+                            <select>
+                                <option disabled selected>select an option</option>
+                                <option>Nursey</option>
+                                <option>L.K.G</option>
+                                <option>U.K.G</option>
+                                <option>Grade-1</option>
+                                <option>Grade-2</option>
+                                <option>Grade-3</option>
+                                <option>Grade-4</option>
+                                <option>Grade-5</option>
+                                <option>Grade-6</option>
+                                <option>Grade-7</option>
+                                <option>Grade-8</option>
+                                <option>Grade-9</option>
+                                <option>Grade-10</option>
+                            </select>
+                        </div>
+                        </div>
+                        {/* admission enquiry second part */}
+                        <div className='adm-enq-third'>
+                            <h1 className='admission-form-con-header'>Personal Information Of Child.</h1>
+                            <div className='adm-enq-third-inner'>
+                                {/* contact info form first div*/}
+                                <div className='contact-info-form'>
+                                    <div className='adm-enq-fst-div1'>
+                                    <label>Name of the Student<span>*</span></label>
+                                    <input type='text'/>
+                                    </div>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Date of Birth<span>*</span></label>
+                                        <input type='text'/>
+                                    </div>
+                                </div>
+                                {/* contact info form second div */}
+                                <div className='contact-info-form-three'>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Age<span>*</span></label>
+                                        <input type='text'/>
+                                    </div>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Place of Birth</label>
+                                        <input type='text'/>
+                                    </div>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Nationality</label>
+                                        <input type='text'/>
+                                    </div>
+                                </div>
+                                 {/* contact info form third div */}
+                                 <div className='contact-info-form-three'>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Name of the present school, if any</label>
+                                        <input type='text'/>
+                                    </div>
+                                 </div>
+                            </div>
+                        </div>
+                        {/* admission enquiry third part */}
+                        <div className='adm-enq-third'>
+                            <h1 className='admission-form-con-header'>Contact Information</h1>
+                            <div className='adm-enq-third-inner'>
+                                {/* contact info form first div*/}
+                                <div className='contact-info-form'>
+                                    <div className='adm-enq-fst-div1'>
+                                    <label>Father Name <span>*</span></label>
+                                    <input type='text'/>
+                                    </div>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Mother Name <span>*</span></label>
+                                        <input type='text'/>
+                                    </div>
+                                </div>
+                                {/* contact info form second div */}
+                                <div className='contact-info-form-three'>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Mobile No.<span>*</span></label>
+                                        <input type='text'/>
+                                    </div>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Alternate Mobile No.</label>
+                                        <input type='text'/>
+                                    </div>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Email-Id<span>*</span></label>
+                                        <input type='text'/>
+                                    </div>
+                                </div>
+                                 {/* contact info form third div */}
+                                 <div className='contact-info-form-three'>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>City<span>*</span></label>
+                                        <input type='text'/>
+                                    </div>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>State</label>
+                                        <input type='text'/>
+                                    </div>
+                                    <div className='adm-enq-fst-div1'>
+                                        <label>Pin Code <span>*</span></label>
+                                        <input type='text'/>
+                                    </div>
+                                 </div>
+                            </div>
+                        </div>
+                        {/*  */}
+                        <div className='admission-submit-btn'>
+                            <button>Submit</button>
+                        </div>
+                        {/*  */}
                     <div>
-                        <label>Academic Year <span>*</span></label>
-                        <select>
-                            <option>2024-2023</option>
-                        </select>
                     </div>
-                    <div>
-                        <label>Class/Standard <span>*</span></label>
-                        <select>
-                            <option disabled selected>select an option</option>
-                            <option>Nursey</option>
-                            <option>L.K.G</option>
-                            <option>U.K.G</option>
-                            <option>Grade-1</option>
-                            <option>Grade-2</option>
-                            <option>Grade-3</option>
-                            <option>Grade-4</option>
-                            <option>Grade-5</option>
-                            <option>Grade-6</option>
-                            <option>Grade-7</option>
-                            <option>Grade-8</option>
-                            <option>Grade-9</option>
-                            <option>Grade-10</option>
-                        </select>
-                    </div>
-
                 </div>
-                <br/>
 
             </div>
         </>
